34dbe2cf4f793c574c32badb2856cdfcdc0ebb91
thetrebleguy <mehslay928@gmail.com>
2026-02-09 14:17:10 +0700

    finally bro

--- 21MergeTwoSortedLists.c
+++ 21MergeTwoSortedLists.c
@@ -8,7 +8,7 @@ struct ListNode {
 };
 
 struct ListNode* createNewNode(int data){
-    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
     newNode->val = data;
     newNode->next = NULL;
     return newNode;
@@ -37,41 +37,58 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     struct ListNode* curr2 = list2;
     int reachedEnd1 = 0, reachedEnd2 = 0;
 
+    // check first
+    if (curr1 == NULL) reachedEnd1 = 1;
+    if (curr2 == NULL) reachedEnd2 = 1;
+
     while (reachedEnd1 == 0 || reachedEnd2 == 0){
-        // check which one is greater
-        printf("%d %d\n", curr1->val, curr2->val);
-        if (curr1->val <= curr2->val && !reachedEnd1){
-            head = appendNode(head, curr1->val);
-            curr1 = curr1->next;
-            if (curr1 == NULL) reachedEnd1 = 1;
-        } else {
-            head = appendNode(head, curr2->val);
-            curr2 = curr2->next;
-            if (curr2 == NULL) reachedEnd2 = 1;
-        };
+        if (!reachedEnd1 && !reachedEnd2){
+            // just check normally
+            if (curr1->val <= curr2->val){
+                head = appendNode(head, curr1->val);
+                curr1 = curr1->next;
+                if (curr1 == NULL) reachedEnd1 = 1;
+            } else {
+                head = appendNode(head, curr2->val);
+                curr2 = curr2->next;
+                if (curr2 == NULL) reachedEnd2 = 1;
+            };
+        } else if (reachedEnd1 && !reachedEnd2){
+            while (!reachedEnd2){
+                head = appendNode(head, curr2->val);
+                curr2 = curr2->next;
+                if (curr2 == NULL) reachedEnd2 = 1;
+            }
+        } else if (!reachedEnd1 && reachedEnd2){
+            while (!reachedEnd1){
+                head = appendNode(head, curr1->val);
+                curr1 = curr1->next;
+                if (curr1 == NULL) reachedEnd1 = 1;
+            }
+        }
     };
 
-    // check by printing
-    struct ListNode* curr = head;
-    while (curr->next != NULL){
-        printf("%d -> ", curr->val);
-        curr = curr->next;
-    };
-    printf("%d -> NULL", curr->val);
+    struct ListNode* current = head;
+    while (current != NULL){
+        printf("%d -> ", current->val);
+        current = current->next;
+    }
+    printf("NULL\n");
+    
     return head;
 };
 
 int main(){
     struct ListNode* head1 = NULL;
     struct ListNode* head2 = NULL;
+    struct ListNode* head = NULL;
     head1 = appendNode(head1, 1);
     head1 = appendNode(head1, 2);
     head1 = appendNode(head1, 4);
     head2 = appendNode(head2, 1);
     head2 = appendNode(head2, 3);
     head2 = appendNode(head2, 4);
-
-    mergeTwoLists(head1, head2); // merge the list here
+    head = mergeTwoLists(head1, head2); // merge the list here
 
     return 0;
 };
