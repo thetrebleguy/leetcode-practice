fbc7f5995df5a5e88fe329011a34ed5feb705c08
thetrebleguy <mehslay928@gmail.com>
2026-02-21 21:00:51 +0700

    ill try this

---
+++ 74SearchA2DMatrix.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// plan to do this 
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    
+};
+
+int main(){
+    return 0;
+};
