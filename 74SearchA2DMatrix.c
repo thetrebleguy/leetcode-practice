e622be79fb27da7122dd7870897eb25fbd682ff0
thetrebleguy <mehslay928@gmail.com>
2026-02-21 21:09:06 +0700

    for tmrw

---
+++ 74SearchA2DMatrix.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// tmrw ill do it la
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    
+}
+
+int main(){
+
+    return 0;
+}
